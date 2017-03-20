5124cc52d267706b3aa51888e9e1810fa0bb785e
janousek77 <janousek77@gmail.com>
2017-03-20 14:25:36 -0700

    added some javascript

--- app.js
+++ app.js
@@ -1,31 +1,47 @@
 'use strict';
 
-var img1 = document.getElementbyId(img1);
-var img2 = document.getElementbyId(img2);
-var img3 = document.getElementbyId(img3);
+var img1 = document.getElementById('img1');
+var img2 = document.getElementById('img2');
+var img3 = document.getElementById('img3');
+var imgArray = [img1, img2, img3];
 
-var bag = new randomImg('bag', 'img/bag.jpg');
-var banana = new randomImg('banana', 'img/banana.jpg');
-var bathroom = new randomImg('bathroom', 'img/bathroom.jpg');
-var boots = new randomImg('boots', 'img/boots.jpg');
-var breakfast = new randomImg('breakfast', 'img/breakfast.jp');
-var bubblegum = new randomImg('bubblegum', 'img/bubblegum.jpg');
-var chair = new randomImg('chair', 'img/chair.jpg');
-var cthulhu = new randomImg('cthulhu', 'img/cthulhu.jpg');
-var dogDuck = new randomImg('dog-duck', 'img/dog-duck.jpg');
-var dragon = new randomImg('dragon', 'img/dragon.jpg');
-var pen = new randomImg('pen', 'img/pen.jpg');
-var petSweep = new randomImg('pet-sweep', 'img/pet-sweep.jpg');
-var scissors = new randomImg('scissors', 'img/scissors.jpg');
-var shark = new randomImg('shark', 'img/shark.jpg');
-var sweep = new randomImg('sweep', 'img/sweep.png');
-var tauntaun = new randomImg('tauntaun', 'img/tauntaun.jpg');
-var unicorn = new randomImg('unicorn','img/unicorn.jpg');
-var usb = new randomImg('usb', 'img/usb.gif');
-var waterCan = new randomImg('water-can', 'img/water-can.jpg');
-var wineGlass = new randomImg('wine-glass','img/wine-glass.jpg');
+var bag = new Image('bag', 'img/bag.jpg');
+var banana = new Image('banana', 'img/banana.jpg');
+var bathroom = new Image('bathroom', 'img/bathroom.jpg');
+var boots = new Image('boots', 'img/boots.jpg');
+var breakfast = new Image('breakfast', 'img/breakfast.jp');
+var bubblegum = new Image('bubblegum', 'img/bubblegum.jpg');
+var chair = new Image('chair', 'img/chair.jpg');
+var cthulhu = new Image('cthulhu', 'img/cthulhu.jpg');
+var dogDuck = new Image('dog-duck', 'img/dog-duck.jpg');
+var dragon = new Image('dragon', 'img/dragon.jpg');
+var pen = new Image('pen', 'img/pen.jpg');
+var petSweep = new Image('pet-sweep', 'img/pet-sweep.jpg');
+var scissors = new Image('scissors', 'img/scissors.jpg');
+var shark = new Image('shark', 'img/shark.jpg');
+var sweep = new Image('sweep', 'img/sweep.png');
+var tauntaun = new Image('tauntaun', 'img/tauntaun.jpg');
+var unicorn = new Image('unicorn','img/unicorn.jpg');
+var usb = new Image('usb', 'img/usb.gif');
+var waterCan = new Image('water-can', 'img/water-can.jpg');
+var wineGlass = new Image('wine-glass','img/wine-glass.jpg');
 
-function randomImg(name, imgPath) {
+var productArray = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass, productArray];
+var pathArray = [];
+
+function Image(name, imgPath) {
   this.name = name;
   this.imgPath = imgPath;
+  pathArray.push(this.imgPath);
+}
+
+function randomImg(){
+  return Math.floor(Math.random() * productArray.length);
+};
+
+function threeImg(){
+  for (var i = 0; i < imgArray.length; i++){
+    var imgChoice = productArray[randomImg()];
+    console.log(imgChoice);
+  }
 }
